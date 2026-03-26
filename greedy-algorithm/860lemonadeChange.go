0513b8084aede2724acb864a6b0edbedb91f1578
Toskies <935936455@qq.com>
2026-03-26 21:20:55 +0800

    greedy 0326

---
+++ greedy-algorithm/860lemonadeChange.go
@@ -0,0 +1,40 @@
+package greedyalgorithm
+
+/*
+https://leetcode.cn/problems/lemonade-change/description/
+
+860. 柠檬水找零
+
+在柠檬水摊上，每一杯柠檬水的售价为 5 美元。顾客排队购买你的产品，（按账单 bills 支付的顺序）一次购买一杯。
+每位顾客只买一杯柠檬水，然后向你付 5 美元、10 美元或 20 美元。你必须给每个顾客正确找零，也就是说净交易是每位顾客向你支付 5 美元。
+注意，一开始你手头没有任何零钱。
+
+给你一个整数数组 bills ，其中 bills[i] 是第 i 位顾客付的账。如果你能给每位顾客正确找零，返回 true ，否则返回 false 。
+*/
+
+func lemonadeChange(bills []int) bool {
+	five := 0
+	ten := 0
+
+	for _, v := range bills {
+		if v == 5 {
+			five++
+		} else if v == 10 {
+			five--
+			ten++
+		} else {
+			if ten > 0 {
+				five--
+				ten--
+			} else {
+				five -= 3
+			}
+		}
+
+		if five < 0 || ten < 0 {
+			return false
+		}
+	}
+
+	return true
+}
